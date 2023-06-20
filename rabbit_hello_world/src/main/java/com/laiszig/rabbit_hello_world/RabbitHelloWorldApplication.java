bd6f32b211115890857ac65fca5412bf34344a52
lais <lais@example.com>
2023-06-20 09:49:04 -0300

    initial commit and port config

---
+++ rabbit_hello_world/src/main/java/com/laiszig/rabbit_hello_world/RabbitHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.laiszig.rabbit_hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitHelloWorldApplication.class, args);
+	}
+
+}
